7a95e3bf50a8055037224243d0fb8e073b041f5c
LEFarcao07 <muhammadandreanfahlefi@gmail.com>
2025-04-29 21:49:53 +0700

    modified:   offline/service-worker.js

--- offline/service-worker.js
+++ offline/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'my-offline-app-v1';
-const OFFLINE_URL = '../offline/offline';
+const OFFLINE_URL = '../offline/offline.html';
 const urlsToCache = [
   '../offline/',
   '../offline/index.html',
@@ -9,44 +9,48 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request).catch(() => {
-          // If fetch fails (offline), return offline page
-          return caches.match('../offline/offline.html');
-        });
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(cache => {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
         })
-      );
-    })
-  );
-});
+    );
+  });
+  
+  self.addEventListener('fetch', event => {
+    // Skip cross-origin requests
+    if (!event.request.url.startsWith(self.location.origin)) {
+      return;
+    }
+  
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          return fetch(event.request).catch(() => {
+            // If fetch fails (offline), return offline page
+            return caches.match(OFFLINE_URL);
+          });
+        })
+    );
+  });
+  
+  self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
